13c0d33d4b8c6118fff459baa486479d920669f9
Microsoft Learn Student <learn@contoso.com>
2020-01-28 16:00:11 +0800

    add init.sh

--- Readme.md
+++ Readme.md
@@ -1,4 +1,5 @@
 # 直接修改odoo.conf 即可。
 
+# 另，要使用git submodule add ./web
 
-# 另，要使用git submolule
+## 直接執行 init.sh
---
+++ init.sh
@@ -0,0 +1,4 @@
+# clone odoo
+git clone -b 12.0 --depth=1 https://github.com/odoo/odoo.git  web
+
+git submodule add ./web
