dedcec15416ba0334ec93b4a5ad0df59bdcf49e8
Microsoft Learn Student <learn@contoso.com>
2020-01-29 16:04:33 +0800

    fixed

--- Readme.md
+++ Readme.md
@@ -1,3 +1,7 @@
+# web 資料夾內缺一Dockerfile, 原本的requirements.txt 也得修改。
+在原本的另一個repository 中有，但是刪了。還好github 可以救回來，謝天謝地，弄那兩個檔案也是很花時間的。
+**那兩個檔案非常重要！**
+
 # 直接修改odoo.conf 即可。
 
 # 另，要使用git submodule add ./web
--- init.sh
+++ init.sh
@@ -2,3 +2,9 @@
 git clone -b 12.0 --depth=1 https://github.com/odoo/odoo.git  web
 
 git submodule add ./web
+rm ./web/requirements.txt
+rm ./web/Dockerfile
+
+cp Dockerfile ./web
+cp requirements.txt ./web
+chmod 777 ./sessions
