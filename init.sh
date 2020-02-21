682435787c13cc2fc54713a5c78210b31def4d72
Microsoft Learn Student <learn@contoso.com>
2020-02-21 09:35:51 +0800

    git trace addons

--- init.sh
+++ init.sh
@@ -1,16 +1,21 @@
 # clone odoo
-git clone -b 12.0 --depth=1 https://github.com/odoo/odoo.git web
+git clone -b 13.0 --depth=1 https://github.com/odoo/odoo.git web
 
 git submodule add ./web
 rm ./web/requirements.txt
-# rm ./web/Dockerfile
-
+rm ./web/Dockerfile
 cp Dockerfile ./web
 cp requirements.txt ./web
+
 mkdir web-data
 chmod 777 ./web-data
 
-mkdir addons
-chmod 777 ./addons
-
 touch .env
+
+ADDONS_PATH="addons"
+mkdir $ADDONS_PATH
+chmod 777 ./$ADDONS_PATH
+cd $ADDONS_PATH
+git init
+cd ..
+git submodule add ./$ADDONS_PATH
